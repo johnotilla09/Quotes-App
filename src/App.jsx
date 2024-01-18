89540edd00dafd93f0498ffeffa25ad004e95274
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-18 23:35:29 +0800

    Quote App | 015 Adding a Layout Wrapper Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import {
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetails from "./pages/QuoteDetails";
 import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <Redirect to="/quotes" />
-            </Route>
-            <Route path="/quotes" exact>
-                <AllQuotes />
-            </Route>
-            <Route path="/quotes/:quoteId">
-                <QuoteDetails />
-            </Route>
-            <Route path="/new-quote">
-                <NewQuote />
-            </Route>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/quotes" />
+                </Route>
+                <Route path="/quotes" exact>
+                    <AllQuotes />
+                </Route>
+                <Route path="/quotes/:quoteId">
+                    <QuoteDetails />
+                </Route>
+                <Route path="/new-quote">
+                    <NewQuote />
+                </Route>
+            </Switch>
+        </Layout>
     );
 }
 
