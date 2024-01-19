d3656754de0a342bf2a1fd2dd00782928a928bde
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-19 17:31:51 +0800

    Quote App | 017 Outputting Data on the _Details_ Page

--- src/components/quotes/QuoteItem.jsx
+++ src/components/quotes/QuoteItem.jsx
@@ -1,19 +1,19 @@
-import classes from './QuoteItem.module.css';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+import classes from "./QuoteItem.module.css";
 
 const QuoteItem = (props) => {
-  return (
-    <li className={classes.item}>
-      <figure>
-        <blockquote>
-          <p>{props.text}</p>
-        </blockquote>
-        <figcaption>{props.author}</figcaption>
-      </figure>
-      <a className='btn'>
-        View Fullscreen
-      </a>
-    </li>
-  );
+    return (
+        <li className={classes.item}>
+            <figure>
+                <blockquote>
+                    <p>{props.text}</p>
+                </blockquote>
+                <figcaption>{props.author}</figcaption>
+            </figure>
+            <Link to={`/quotes/${props.id}`} className="btn">View Fullscreen</Link>
+        </li>
+    );
 };
 
 export default QuoteItem;
--- src/pages/QuoteDetails.jsx
+++ src/pages/QuoteDetails.jsx
@@ -2,16 +2,39 @@ import { Fragment } from "react";
 
 import { useParams, Route } from "react-router-dom/cjs/react-router-dom.min";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {
+        id: 1,
+        author: "Dolly Parton",
+        text: "The way I see it, if you want the rainbow, you gotta put up with the rain.",
+    },
+    {
+        id: 2,
+        author: "Joe E. Lewis",
+        text: "You only live once, but if you work it right, once is enough.",
+    },
+];
 
 const QuoteDetails = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find(
+        (quote) => quote.id === Number(params.quoteId)
+    );
+
+    if (!quote) {
+        return (
+            <p>No quote found!</p>
+        );
+    }
+
     return (
         <Fragment>
-            <h1>Quote Details</h1>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments/>
+                <Comments />
             </Route>
         </Fragment>
     );
