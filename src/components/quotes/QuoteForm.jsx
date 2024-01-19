35adafccb49f8c4994aa944c4179c634bcda7cfb
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-19 22:39:48 +0800

    Quote App | 020 Preventing Possibly Unwanted Route Transitions with the _Prompt_ Component

--- src/components/quotes/QuoteForm.jsx
+++ src/components/quotes/QuoteForm.jsx
@@ -1,12 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState, Fragment } from "react";
+import { Prompt } from "react-router-dom/cjs/react-router-dom.min";
 
 import Card from "../UI/Card";
 import LoadingSpinner from "../UI/LoadingSpinner";
-import classes from "./QuoteForm.module.css";
+import classes from "./QuoteForm.module.css"
 
 const QuoteForm = (props) => {
     const authorInputRef = useRef();
     const textInputRef = useRef();
+    const [isEntering, setIsEntering] = useState(false);
+
+    const formFocusHandler = () => {
+        setIsEntering(true);
+    };
+
+    const finishEnteringHandler = () => {
+        setIsEntering(false);
+    }
 
     function submitFormHandler(event) {
         event.preventDefault();
@@ -20,27 +30,44 @@ const QuoteForm = (props) => {
     }
 
     return (
-        <Card>
-            <form className={classes.form} onSubmit={submitFormHandler}>
-                {props.isLoading && (
-                    <div className={classes.loading}>
-                        <LoadingSpinner />
-                    </div>
-                )}
+        <Fragment>
+            <Prompt
+                when={isEntering}
+                message={(location) =>
+                    "Are you sure you want to leave? All your entered data will be lost!"
+                }
+            />
 
-                <div className={classes.control}>
-                    <label htmlFor="author">Author</label>
-                    <input type="text" id="author" ref={authorInputRef} />
-                </div>
-                <div className={classes.control}>
-                    <label htmlFor="text">Text</label>
-                    <textarea id="text" rows="5" ref={textInputRef}></textarea>
-                </div>
-                <div className={classes.actions}>
-                    <button className="btn">Add Quote</button>
-                </div>
-            </form>
-        </Card>
+            <Card>
+                <form
+                    onFocus={formFocusHandler}
+                    className={classes.form}
+                    onSubmit={submitFormHandler}
+                >
+                    {props.isLoading && (
+                        <div className={classes.loading}>
+                            <LoadingSpinner />
+                        </div>
+                    )}
+
+                    <div className={classes.control}>
+                        <label htmlFor="author">Author</label>
+                        <input type="text" id="author" ref={authorInputRef} />
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="text">Text</label>
+                        <textarea
+                            id="text"
+                            rows="5"
+                            ref={textInputRef}
+                        ></textarea>
+                    </div>
+                    <div className={classes.actions}>
+                        <button onClick={finishEnteringHandler} className="btn">Add Quote</button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
     );
 };
 
