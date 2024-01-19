5511f6ed6bde7e491e2cc771aa8d18a0be22c748
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-19 20:49:34 +0800

    Quote App | 019 Implementing Programmatic (Imperative) Navigation

--- src/pages/NewQuote.jsx
+++ src/pages/NewQuote.jsx
@@ -1,8 +1,18 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+    const history = useHistory();
+
+    const addQuoteHandler = (data) => {
+        console.log(data);
+
+        history.push('/quotes');
+    }
+
     return (
-        <QuoteForm />
+        <QuoteForm onAddQuote={addQuoteHandler}/>
     );
 }
 
