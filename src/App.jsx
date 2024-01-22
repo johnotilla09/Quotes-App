de9e51c426f2da012c42beca85a903c8f7af1d5d
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2024-01-22 21:25:30 +0800

    Qote App | Apply lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import {
-    Route,
-    Switch,
-    Redirect,
-} from "react-router-dom/cjs/react-router-dom.min";
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetails from "./pages/QuoteDetails";
-import NewQuote from "./pages/NewQuote";
+import React, { Suspense } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+// import AllQuotes from "./pages/AllQuotes";
+// import QuoteDetails from "./pages/QuoteDetails";
+// import NewQuote from "./pages/NewQuote";
+
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = React.lazy(() => import('./pages/QuoteDetails'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
     return (
         <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/quotes" />
-                </Route>
-                <Route path="/quotes" exact>
-                    <AllQuotes />
-                </Route>
-                <Route path="/quotes/:quoteId">
-                    <QuoteDetails />
-                </Route>
-                <Route path="/new-quote">
-                    <NewQuote />
-                </Route>
-                <Route path='*'>
-                    <NotFound/>
-                </Route>
-            </Switch>
-
+            <Suspense
+                fallback={
+                    <div className="centered">
+                        <LoadingSpinner />
+                    </div>
+                }
+            >
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/quotes" />
+                    </Route>
+                    <Route path="/quotes" exact>
+                        <AllQuotes />
+                    </Route>
+                    <Route path="/quotes/:quoteId">
+                        <QuoteDetails />
+                    </Route>
+                    <Route path="/new-quote">
+                        <NewQuote />
+                    </Route>
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </Suspense>
             <footer className="footer">
                 <p>Alright Reserved 2024</p>
             </footer>
